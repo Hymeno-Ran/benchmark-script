29a3830bff01ee8b9af88fbbf008d91070cf3c32
Hiroto <jaydenthai16@gmail.com>
2024-11-24 20:48:45 +0700

    feat: update echo

--- yabs.sh
+++ yabs.sh
@@ -1029,6 +1029,7 @@ if [ $CPUCores -gt 1 ]; then
 else
 	# Run 7-zip benchmark multi-threaded
 	echo -e "Running multi-threaded benchmark..."
+	echo -e "Tottal Core:${CPUCore} "
 	taskset -c 0-$((CPUCores-1)) "${SEVENZIP}" b ${DictSize} -mmt=${CPUCores}
 fi
 
